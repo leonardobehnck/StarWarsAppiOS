6959fb217b5f033e7a6808d4556568bdb3bfa141
Leonardo Behnck <89857767+leonardobehnck@users.noreply.github.com>
2024-11-21 11:21:21 -0300

    Initial Commit

---
+++ StarWarsApp/StarWarsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsAppApp.swift
+//  StarWarsApp
+//
+//  Created by Leonardo Behnck on 21/11/24.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
